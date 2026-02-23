116bb7ca5fc796cd21022a09f2d7b5452f59cf4a
Hammam Elsheikh <hammam.elsheikh@gmail.com>
2026-02-23 23:21:57 +0200

    2.7: The Phonebook Step 2

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,36 +1,41 @@
-import { useState } from "react";
+import {useState} from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
-  const [newName, setNewName] = useState("");
-  function handleInputChange(e) {
-    setNewName(e.target.value);
-  }
-  function addName(e) {
-    e.preventDefault();
-    setPersons([...persons, { name: newName }]);
-  }
+    const [persons, setPersons] = useState([{name: "Arto Hellas"}]);
+    const [newName, setNewName] = useState("");
+
+    function handleInputChange(e) {
+        setNewName(e.target.value);
+    }
+
+    function addName(e) {
+        e.preventDefault();
+        if(persons.findIndex(person=>person.name === newName) === -1)
+         setPersons([...persons, {name: newName}]);
+        else alert(`${newName} is already added to phonebook`)
+    }
+
     return (
-    <div>
-      <h2>Phonebook</h2>
-      <form onSubmit={addName}>
         <div>
-          name: <input value={newName} onChange={handleInputChange} />
+            <h2>Phonebook</h2>
+            <form onSubmit={addName}>
+                <div>
+                    name: <input value={newName} onChange={handleInputChange}/>
+                </div>
+                <div>
+                    <button type="submit">
+                        add
+                    </button>
+                </div>
+            </form>
+            <h2>Numbers</h2>
+            <ul>
+                {persons.map((person) => (
+                    <li key={person.name}> {person.name}</li>
+                ))}
+            </ul>
         </div>
-        <div>
-          <button type="submit" >
-            add
-          </button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      <ul>
-        {persons.map((person) => (
-          <li key={person.name}> {person.name}</li>
-        ))}
-      </ul>
-    </div>
-  );
+    );
 };
 
 export default App;
