d5fc882a7bd0b16df8ae531b82a9a1d80852759a
Hammam Elsheikh <hammam.elsheikh@gmail.com>
2026-02-23 17:01:43 +0200

    2.1: Course information step 6

--- part1/course-info/src/App.jsx
+++ part1/course-info/src/App.jsx
@@ -1,63 +1,55 @@
 import "./App.css";
-
-function App() {
+const App = () => {
   const course = {
+    id: 1,
     name: "Half Stack application development",
     parts: [
       {
         name: "Fundamentals of React",
         exercises: 10,
+        id: 1,
       },
       {
         name: "Using props to pass data",
         exercises: 7,
+        id: 2,
       },
       {
         name: "State of a component",
         exercises: 14,
+        id: 3,
       },
     ],
   };
 
-  return (
-    <div>
-      <Header title={course} />
-      <Content
-        part1={course.parts[0].name}
-        part2={course.parts[1].name}
-        part3={course.parts[2].name}
-        exercises1={course.parts[0].exercises}
-        exercises2={course.parts[1].exercises}
-        exercises3={course.parts[2].exercises}
-      />
-      <Total
-        exercises1={course.parts[0].exercises}
-        exercises2={course.parts[1].exercises}
-        exercises3={course.parts[2].exercises}
-      />
-    </div>
-  );
-}
+  return <Course course={course} />;
+};
 
 export default App;
 
-function Header({ course }) {
-  return <h1>{course}</h1>;
-}
-
-function Content({ exercises1, exercises2, exercises3, part1, part2, part3 }) {
+const Course = ({ course }) => {
   return (
     <>
-      <Part part={part1} exercises={exercises1} />
-      <Part part={part2} exercises={exercises2} />
-      <Part part={part3} exercises={exercises3} />
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+    </>
+  );
+};
+
+function Header({ name }) {
+  return <h1>{name}</h1>;
+}
+
+function Content({ parts }) {
+  return (
+    <>
+      {parts.map((part) => (
+        <Part key={part.id} part={part.name} exercises={part.exercises} />
+      ))}
     </>
   );
 }
 
-function Total({ exercises1, exercises2, exercises3 }) {
-  return <p> Number of exercises {exercises1 + exercises2 + exercises3}</p>;
-}
 function Part({ part, exercises }) {
   return (
     <>
@@ -67,3 +59,7 @@ function Part({ part, exercises }) {
     </>
   );
 }
+
+// function Total({ exercises1, exercises2, exercises3 }) {
+//   return <p> Number of exercises {exercises1 + exercises2 + exercises3}</p>;
+// }
