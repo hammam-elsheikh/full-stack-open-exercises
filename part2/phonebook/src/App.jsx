27acd499534fb77a36463fe56673f99b0a4d64b5
Hammam Elsheikh <hammam.elsheikh@gmail.com>
2026-02-23 23:42:25 +0200

    2.8: The Phonebook Step 3

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,41 +1,58 @@
 import {useState} from "react";
 
 const App = () => {
-    const [persons, setPersons] = useState([{name: "Arto Hellas"}]);
+    const [persons, setPersons] = useState([{
+        name: "Arto Hellas", phone: '01234567891'
+    }]);
     const [newName, setNewName] = useState("");
+    const [newNumber, setNewNumber] = useState('');
 
-    function handleInputChange(e) {
+    function handleNameChange(e) {
         setNewName(e.target.value);
     }
 
-    function addName(e) {
+    function handleNumberChange(e) {
+        setNewNumber(e.target.value);
+    }
+
+    function addRecord(e) {
         e.preventDefault();
-        if(persons.findIndex(person=>person.name === newName) === -1)
-         setPersons([...persons, {name: newName}]);
+        if (persons.findIndex(person => person.name === newName) === -1)
+            setPersons([...persons, {name: newName, phone: newNumber}]);
         else alert(`${newName} is already added to phonebook`)
     }
 
-    return (
-        <div>
-            <h2>Phonebook</h2>
-            <form onSubmit={addName}>
-                <div>
-                    name: <input value={newName} onChange={handleInputChange}/>
-                </div>
-                <div>
-                    <button type="submit">
-                        add
-                    </button>
-                </div>
-            </form>
-            <h2>Numbers</h2>
-            <ul>
-                {persons.map((person) => (
-                    <li key={person.name}> {person.name}</li>
-                ))}
-            </ul>
-        </div>
-    );
+    return (<>
+        <h2>Phonebook</h2>
+        <form onSubmit={addRecord}>
+            <table>
+                <tbody>
+                <tr>
+                    <td> name:</td>
+                    <td><input required={true} value={newName} onChange={handleNameChange}/></td>
+                </tr>
+                <tr>
+                    <td>number:</td>
+                    <td><input required={true} value={newNumber} onChange={handleNumberChange}/></td>
+                </tr>
+                </tbody>
+            </table>
+            <div>
+                <button type="submit">
+                    add
+                </button>
+            </div>
+        </form>
+        <h2>Numbers</h2>
+        <table>
+            <tbody>
+            {persons.map((person) => (<tr key={person.name}>
+                <td>{person.name}</td>
+                <td>{person.phone}</td>
+            </tr>))}
+            </tbody>
+        </table>
+    </>);
 };
 
 export default App;
