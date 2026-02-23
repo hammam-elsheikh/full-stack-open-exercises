07b8b139d69e0a80d5bf455f0c2f7933232d171e
Hammam Elsheikh <hammam.elsheikh@gmail.com>
2026-02-23 07:39:21 +0200

    1.1: Course Information, step 1

---
+++ part1/course-info/src/App.jsx
@@ -0,0 +1,56 @@
+import "./App.css";
+
+function App() {
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
+
+  return (
+    <div>
+      <Header title={course} />
+      <Content
+        part1={part1}
+        part2={part2}
+        part3={part3}
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
+      />
+      <Total
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
+      />
+    </div>
+  );
+}
+
+export default App;
+
+function Header({ course }) {
+  return <h1>{course}</h1>;
+}
+
+function Content({ exercises1, exercises2, exercises3, part1, part2, part3 }) {
+  return (
+    <>
+      <p>
+        {part1} {exercises1}
+      </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p>
+    </>
+  );
+}
+
+function Total({ exercises1, exercises2, exercises3 }) {
+  return <p> Number of exercises {exercises1 + exercises2 + exercises3}</p>;
+}
