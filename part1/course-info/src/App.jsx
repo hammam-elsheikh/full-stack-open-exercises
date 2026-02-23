fcadd887b6cbf022fde15a043029ecf3d83bd059
Hammam Elsheikh <hammam.elsheikh@gmail.com>
2026-02-23 07:41:44 +0200

    1.2: Course Information, step 2

--- part1/course-info/src/App.jsx
+++ part1/course-info/src/App.jsx
@@ -38,19 +38,22 @@ function Header({ course }) {
 function Content({ exercises1, exercises2, exercises3, part1, part2, part3 }) {
   return (
     <>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
+      <Part part={part1} exercises={exercises1} />
+      <Part part={part2} exercises={exercises2} />
+      <Part part={part3} exercises={exercises3} />
     </>
   );
 }
 
 function Total({ exercises1, exercises2, exercises3 }) {
   return <p> Number of exercises {exercises1 + exercises2 + exercises3}</p>;
+}
+function Part({ part, exercises }) {
+  return (
+    <>
+      <p>
+        {part} {exercises}
+      </p>
+    </>
+  );
 }
