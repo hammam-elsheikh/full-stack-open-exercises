c414ebf7b404adbdd4b2a3542f76de37cb6b16a4
Hammam Elsheikh <hammam.elsheikh@gmail.com>
2026-02-23 05:26:30 +0200

    1.7: unicafe step 2

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,30 @@
+/* eslint-disable no-unused-vars */
+import { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const all = good + neutral + bad;
+  const average = all === 0 ? 0 : (good - bad) / all;
+  const positive = all === 0 ? 0 : (good / all) * 100;
+
+  return (
+    <>
+      <h1>give feedback</h1>
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <h2>statistics</h2>
+      <p>good: {good}</p>
+      <p>neutral: {neutral}</p>
+      <p>bad: {bad}</p>
+      <p>all: {all}</p>
+      <p>average: {average}</p>
+      <p>positive: {positive} %</p>
+    </>
+  );
+}
+
+export default App;
